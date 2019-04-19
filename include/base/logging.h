43de0cfb7b0fbe876761111a55452fe954fc4103
LinLiting <linliting06@live.com>
2019-04-19 19:35:31 +0800

    2019-4-19

--- include/base/logging.h
+++ include/base/logging.h
@@ -179,10 +179,117 @@ _ENSURE_EQ_4(leftExp, rightExp, Base::ErrorCodeType::USERDEFINED, errorCode)
 #define _ENSURE_EQ_2(leftExp, rightExp) \
 _ENSURE_EQ_3(leftExp, rightExp, -1)
 
-#define ENSURE_WIN32API(condition) \
-_ENSURE_3(condition, Base::ErrorCodeType::WIN32API, GetLastError()) << Base::getWin32LastErrorString()
+#define ENSURE_NE_WITH_HANDLER(...) _PP_MACRO_OVERLOAD(_ENSURE_NE_WITH_HANDLER, __VA_ARGS__)
+#define _ENSURE_NE_WITH_HANDLER_5(leftExp, rightExp, errorCodeType, errorCode, handler) \
+_LOG_CONDITIONED_BINARY_OP_NE_GENERIC(leftExp, rightExp, Base::FatalErrorLogging, errorCodeType, errorCode, handler)
+#define _ENSURE_NE_WITH_HANDLER_4(leftExp, rightExp, errorCode, handler) \
+_ENSURE_NE_WITH_HANDLER_5(leftExp, rightExp, Base::ErrorCodeType::USERDEFINED, errorCode, handler)
+#define _ENSURE_NE_WITH_HANDLER_3(leftExp, rightExp, handler) \
+_ENSURE_NE_WITH_HANDLER_4(leftExp, rightExp, -1, handler)
 
-#define ENSURE_HR(condition)
+#define ENSURE_NE(...) _PP_MACRO_OVERLOAD(_ENSURE_NE, __VA_ARGS__)
+#define _ENSURE_NE_4(leftExp, rightExp, errorCodeType, errorCode) \
+_ENSURE_NE_WITH_HANDLER_5(leftExp, rightExp, errorCodeType, errorCode, nullptr)
+#define _ENSURE_NE_3(leftExp, rightExp, errorCode) \
+_ENSURE_NE_4(leftExp, rightExp, Base::ErrorCodeType::USERDEFINED, errorCode)
+#define _ENSURE_NE_2(leftExp, rightExp) \
+_ENSURE_NE_3(leftExp, rightExp, -1)
+
+#define ENSURE_GE_WITH_HANDLER(...) _PP_MACRO_OVERLOAD(_ENSURE_GE_WITH_HANDLER, __VA_ARGS__)
+#define _ENSURE_GE_WITH_HANDLER_5(leftExp, rightExp, errorCodeType, errorCode, handler) \
+_LOG_CONDITIONED_BINARY_OP_GE_GENERIC(leftExp, rightExp, Base::FatalErrorLogging, errorCodeType, errorCode, handler)
+#define _ENSURE_GE_WITH_HANDLER_4(leftExp, rightExp, errorCode, handler) \
+_ENSURE_GE_WITH_HANDLER_5(leftExp, rightExp, Base::ErrorCodeType::USERDEFINED, errorCode, handler)
+#define _ENSURE_GE_WITH_HANDLER_3(leftExp, rightExp, handler) \
+_ENSURE_GE_WITH_HANDLER_4(leftExp, rightExp, -1, handler)
+
+#define ENSURE_GE(...) _PP_MACRO_OVERLOAD(_ENSURE_GE, __VA_ARGS__)
+#define _ENSURE_GE_4(leftExp, rightExp, errorCodeType, errorCode) \
+_ENSURE_GE_WITH_HANDLER_5(leftExp, rightExp, errorCodeType, errorCode, nullptr)
+#define _ENSURE_GE_3(leftExp, rightExp, errorCode) \
+_ENSURE_GE_4(leftExp, rightExp, Base::ErrorCodeType::USERDEFINED, errorCode)
+#define _ENSURE_GE_2(leftExp, rightExp) \
+_ENSURE_GE_3(leftExp, rightExp, -1)
+
+#define ENSURE_GT_WITH_HANDLER(...) _PP_MACRO_OVERLOAD(_ENSURE_GT_WITH_HANDLER, __VA_ARGS__)
+#define _ENSURE_GT_WITH_HANDLER_5(leftExp, rightExp, errorCodeType, errorCode, handler) \
+_LOG_CONDITIONED_BINARY_OP_GT_GENERIC(leftExp, rightExp, Base::FatalErrorLogging, errorCodeType, errorCode, handler)
+#define _ENSURE_GT_WITH_HANDLER_4(leftExp, rightExp, errorCode, handler) \
+_ENSURE_GT_WITH_HANDLER_5(leftExp, rightExp, Base::ErrorCodeType::USERDEFINED, errorCode, handler)
+#define _ENSURE_GT_WITH_HANDLER_3(leftExp, rightExp, handler) \
+_ENSURE_GT_WITH_HANDLER_4(leftExp, rightExp, -1, handler)
+
+#define ENSURE_GT(...) _PP_MACRO_OVERLOAD(_ENSURE_GT, __VA_ARGS__)
+#define _ENSURE_GT_4(leftExp, rightExp, errorCodeType, errorCode) \
+_ENSURE_GT_WITH_HANDLER_5(leftExp, rightExp, errorCodeType, errorCode, nullptr)
+#define _ENSURE_GT_3(leftExp, rightExp, errorCode) \
+_ENSURE_GT_4(leftExp, rightExp, Base::ErrorCodeType::USERDEFINED, errorCode)
+#define _ENSURE_GT_2(leftExp, rightExp) \
+_ENSURE_GT_3(leftExp, rightExp, -1)
+
+#define ENSURE_LE_WITH_HANDLER(...) _PP_MACRO_OVERLOAD(_ENSURE_LE_WITH_HANDLER, __VA_ARGS__)
+#define _ENSURE_LE_WITH_HANDLER_5(leftExp, rightExp, errorCodeType, errorCode, handler) \
+_LOG_CONDITIONED_BINARY_OP_LE_GENERIC(leftExp, rightExp, Base::FatalErrorLogging, errorCodeType, errorCode, handler)
+#define _ENSURE_LE_WITH_HANDLER_4(leftExp, rightExp, errorCode, handler) \
+_ENSURE_LE_WITH_HANDLER_5(leftExp, rightExp, Base::ErrorCodeType::USERDEFINED, errorCode, handler)
+#define _ENSURE_LE_WITH_HANDLER_3(leftExp, rightExp, handler) \
+_ENSURE_LE_WITH_HANDLER_4(leftExp, rightExp, -1, handler)
+
+#define ENSURE_LE(...) _PP_MACRO_OVERLOAD(_ENSURE_LE, __VA_ARGS__)
+#define _ENSURE_LE_4(leftExp, rightExp, errorCodeType, errorCode) \
+_ENSURE_LE_WITH_HANDLER_5(leftExp, rightExp, errorCodeType, errorCode, nullptr)
+#define _ENSURE_LE_3(leftExp, rightExp, errorCode) \
+_ENSURE_LE_4(leftExp, rightExp, Base::ErrorCodeType::USERDEFINED, errorCode)
+#define _ENSURE_LE_2(leftExp, rightExp) \
+_ENSURE_LE_3(leftExp, rightExp, -1)
+
+#define ENSURE_LT_WITH_HANDLER(...) _PP_MACRO_OVERLOAD(_ENSURE_LT_WITH_HANDLER, __VA_ARGS__)
+#define _ENSURE_LT_WITH_HANDLER_5(leftExp, rightExp, errorCodeType, errorCode, handler) \
+_LOG_CONDITIONED_BINARY_OP_LT_GENERIC(leftExp, rightExp, Base::FatalErrorLogging, errorCodeType, errorCode, handler)
+#define _ENSURE_LT_WITH_HANDLER_4(leftExp, rightExp, errorCode, handler) \
+_ENSURE_LT_WITH_HANDLER_5(leftExp, rightExp, Base::ErrorCodeType::USERDEFINED, errorCode, handler)
+#define _ENSURE_LT_WITH_HANDLER_3(leftExp, rightExp, handler) \
+_ENSURE_LT_WITH_HANDLER_4(leftExp, rightExp, -1, handler)
+
+#define ENSURE_LT(...) _PP_MACRO_OVERLOAD(_ENSURE_LT, __VA_ARGS__)
+#define _ENSURE_LT_4(leftExp, rightExp, errorCodeType, errorCode) \
+_ENSURE_LT_WITH_HANDLER_5(leftExp, rightExp, errorCodeType, errorCode, nullptr)
+#define _ENSURE_LT_3(leftExp, rightExp, errorCode) \
+_ENSURE_LT_4(leftExp, rightExp, Base::ErrorCodeType::USERDEFINED, errorCode)
+#define _ENSURE_LT_2(leftExp, rightExp) \
+_ENSURE_LT_3(leftExp, rightExp, -1)
+
+#define ENSURE_WIN32API_WITH_HANDLER(condition, handler) \
+_ENSURE_WITH_HANDLER_4(condition, Base::ErrorCodeType::WIN32API, GetLastError(), handler) << Base::getWin32LastErrorString()
+#define ENSURE_WIN32API(condition) \
+ENSURE_WIN32API_WITH_HANDLER(condition, nullptr)
+#define ENSURE_EQ_WIN32API_WITH_HANDLER(leftExp, rightExp, handler) \
+_ENSURE_EQ_WITH_HANDLER_5(leftExp, rightExp, Base::ErrorCodeType::WIN32API, GetLastError(), handler) << Base::getWin32LastErrorString()
+#define ENSURE_EQ_WIN32API(leftExp, rightExp) \
+ENSURE_EQ_WIN32API_WITH_HANDLER(leftExp, rightExp, nullptr)
+#define ENSURE_NE_WIN32API_WITH_HANDLER(leftExp, rightExp, handler) \
+_ENSURE_NE_WITH_HANDLER_5(leftExp, rightExp, Base::ErrorCodeType::WIN32API, GetLastError(), handler) << Base::getWin32LastErrorString()
+#define ENSURE_NE_WIN32API(leftExp, rightExp) \
+ENSURE_NE_WIN32API_WITH_HANDLER(leftExp, rightExp, nullptr)
+#define ENSURE_GE_WIN32API_WITH_HANDLER(leftExp, rightExp, handler) \
+_ENSURE_GE_WITH_HANDLER_5(leftExp, rightExp, Base::ErrorCodeType::WIN32API, GetLastError(), handler) << Base::getWin32LastErrorString()
+#define ENSURE_GE_WIN32API(leftExp, rightExp) \
+ENSURE_GE_WIN32API_WITH_HANDLER(leftExp, rightExp, nullptr)
+#define ENSURE_GT_WIN32API_WITH_HANDLER(leftExp, rightExp, handler) \
+_ENSURE_GT_WITH_HANDLER_5(leftExp, rightExp, Base::ErrorCodeType::WIN32API, GetLastError(), handler) << Base::getWin32LastErrorString()
+#define ENSURE_GT_WIN32API(leftExp, rightExp) \
+ENSURE_GT_WIN32API_WITH_HANDLER(leftExp, rightExp, nullptr)
+#define ENSURE_LE_WIN32API_WITH_HANDLER(leftExp, rightExp, handler) \
+_ENSURE_LE_WITH_HANDLER_5(leftExp, rightExp, Base::ErrorCodeType::WIN32API, GetLastError(), handler) << Base::getWin32LastErrorString()
+#define ENSURE_LE_WIN32API(leftExp, rightExp) \
+ENSURE_LE_WIN32API_WITH_HANDLER(leftExp, rightExp, nullptr)
+#define ENSURE_LT_WIN32API_WITH_HANDLER(leftExp, rightExp, handler) \
+_ENSURE_LT_WITH_HANDLER_5(leftExp, rightExp, Base::ErrorCodeType::WIN32API, GetLastError(), handler) << Base::getWin32LastErrorString()
+#define ENSURE_LT_WIN32API(leftExp, rightExp) \
+ENSURE_LT_WIN32API_WITH_HANDLER(leftExp, rightExp, nullptr)
+
+#define ENSURE_HR_WITH_HANDLER(condition, handler) \
+_ENSURE_GE_WITH_HANDLER_5(condition, 0, Base::ErrorCodeType::HRESULT, )
 
 #define ENSURE_HR(exp) \
 	if (HRESULT _hr = (exp)) if (_hr < 0) \
