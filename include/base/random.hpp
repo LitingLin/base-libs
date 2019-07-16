28873c054eeaf63a7ddba1beb314635563f50f1c
LinLiting <linliting06@live.com>
2019-07-16 21:43:56 +0800

    2019-7-16

--- include/base/random.hpp
+++ include/base/random.hpp
@@ -10,7 +10,7 @@ namespace Base
 	{
 	public:
 		// [a, b]
-		template <typename Type, typename
+		template <typename Type,
 			std::enable_if_t<std::is_same_v<Type, short> || std::is_same_v<Type, int> || std::is_same_v<Type, long> ||
 			std::is_same_v<Type, long long> || std::is_same_v<Type, unsigned short> || std::is_same_v<Type, unsigned int> || std::is_same_v<Type, unsigned long> ||
 			std::is_same_v<Type, unsigned long long>>* = nullptr>
@@ -21,7 +21,7 @@ namespace Base
 		}
 
 		// [a, b]
-		template <typename Type, typename
+		template <typename Type,
 			std::enable_if_t<std::numeric_limits<Type>::is_integer && std::is_signed_v<Type> &&
 			!(std::is_same_v<Type, short> || std::is_same_v<Type, int> || std::is_same_v<Type, long> || std::is_same_v<Type, long long>)>* = nullptr>
 			Type get(Type min = std::numeric_limits<Type>::min(), Type max = std::numeric_limits<Type>::max())
@@ -31,8 +31,8 @@ namespace Base
 		}
 
 		// [a, b]
-		template <typename Type, typename
-			std::enable_if_t<std::numeric_limits<Type>::is_integer&& std::is_unsigned_v<Type> &&
+		template <typename Type,
+			std::enable_if_t<std::numeric_limits<Type>::is_integer && std::is_unsigned_v<Type> &&
 			!(std::is_same_v<Type, unsigned short> || std::is_same_v<Type, unsigned int> || std::is_same_v<Type, unsigned long> ||
 				std::is_same_v<Type, unsigned long long>)>* = nullptr>
 			Type get(Type min = std::numeric_limits<Type>::min(), Type max = std::numeric_limits<Type>::max())
@@ -43,18 +43,18 @@ namespace Base
 
 		// [a, b)
 		// To create a distribution over the closed interval [a,b], std::nextafter(b, std::numeric_limits<RealType>::max()) may be used as the second parameter.
-		template <typename Type, typename
+		template <typename Type,
 			std::enable_if_t<std::is_floating_point<Type>::value>* = nullptr>
-		Type get(Type min = std::numeric_limits<Type>::min(), Type max = std::numeric_limits<Type>::max())
+			Type get(Type min = std::numeric_limits<Type>::min(), Type max = std::numeric_limits<Type>::max())
 		{
 			std::uniform_real_distribution<Type> distribution(min, max);
 			return distribution(_engine);
 		}
 
-		template <typename Type>
-		Type get_normal(Type min = std::numeric_limits<Type>::min(), Type max = std::numeric_limits<Type>::max())
+		template <typename RealType>
+		RealType get_normal(RealType min = std::numeric_limits<RealType>::min(), RealType max = std::numeric_limits<RealType>::max())
 		{
-			std::uniform_int_distribution<Type> distribution(min, max);
+			std::normal_distribution<RealType> distribution(min, max);
 			return distribution(_engine);
 		}
 	private:
