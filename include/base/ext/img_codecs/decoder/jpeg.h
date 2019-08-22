03067fe448c3a7bb6e19e516fbde7dfec0e16cf1
Liting Lin <linliting06@live.com>
2019-08-23 03:35:34 +0800

    2019-8-22

--- include/base/ext/img_codecs/decoder/jpeg.h
+++ include/base/ext/img_codecs/decoder/jpeg.h
@@ -1,13 +1,36 @@
 #pragma once
 
-#ifdef HAVE_LIB_JPEG
+#include <base/ext/img_codecs/common.h>
 
+#ifdef HAVE_LIB_JPEG_TURBO
+#include <turbojpeg.h>
+#include <cstdint>
+namespace Base {
+    class IMAGE_CODECS_INTERFACE JPEGDecoder {
+    public:
+        JPEGDecoder();
+        JPEGDecoder(const JPEGDecoder &) = delete;
+        JPEGDecoder(JPEGDecoder &&object) noexcept;
+        ~JPEGDecoder();
+        void load(const void *pointer, uint64_t size);
+        int getWidth();
+        int getHeight();
+        int getDecompressedSize();
+        void decode(void *buffer);
+    private:
+        const unsigned char *_pointer;
+        unsigned long _fileSize;
+        tjhandle _handle;
+        int _width, _height;
+        int _jpegSubsamp, _jpegColorspace;
+    };
+}
+#elif defined HAVE_LIB_JPEG
 #ifdef _WIN32
 #define NOMINMAX
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #endif
-#include <base/ext/img_codecs/common.h>
 #include <stdio.h>
 #include <jpeglib.h>
 #include <cstdint>
