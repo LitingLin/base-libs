2855c99182f4806d3727cfbc454e91e7e68aeb91
LinLiting <linliting06@live.com>
2019-09-23 21:02:24 +0800

    2019-9-23

--- src/base/ext/img_codecs/decoder/jpeg_intel_media_sdk.cpp
+++ src/base/ext/img_codecs/decoder/jpeg_intel_media_sdk.cpp
@@ -2,10 +2,19 @@
 
 #include <base/logging.h>
 
-extern "C" {
-#include <libavutil/imgutils.h>
-#include <libswscale/swscale.h>
-}
+#ifdef _MSC_VER
+#pragma warning(push, 0)
+#elif defined __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wall"
+#endif
+#include <mfxjpeg.h>
+#include <iostream>
+#ifdef _MSC_VER
+#pragma warning(pop)
+#elif defined __GNUC__
+#pragma GCC diagnostic pop
+#endif
 
 namespace Base
 {
@@ -14,7 +23,7 @@ namespace Base
 	{
 		mfxVersion version;
 		version.Major = 1;
-		version.Minor = 0;
+		version.Minor = 3;
 		L_CHECK_EQ(MFXInit(MFX_IMPL_HARDWARE_ANY, &version, &_session), MFX_ERR_NONE);
 	}
 
@@ -36,8 +45,29 @@ namespace Base
 		bitstream.DataFlag = MFX_BITSTREAM_COMPLETE_FRAME;
 
 		memset(&_param, 0, sizeof(_param));
-		L_CHECK_EQ(MFXVideoDECODE_DecodeHeader(_session, &bitstream, &_param), MFX_ERR_NONE);
-		L_CHECK_EQ(MFXVideoDECODE_Init(_session, &_param), MFX_ERR_NONE);
+		_param.mfx.CodecId = MFX_CODEC_JPEG;
+		_param.IOPattern = MFX_IOPATTERN_IN_SYSTEM_MEMORY | MFX_IOPATTERN_OUT_SYSTEM_MEMORY;
+		auto decodingStatus = MFXVideoDECODE_DecodeHeader(_session, &bitstream, &_param);
+		if(decodingStatus == MFX_WRN_PARTIAL_ACCELERATION)
+		{
+			;
+		}
+		else
+		{
+			L_CHECK_EQ(decodingStatus, MFX_ERR_NONE);
+		}
+				
+		L_CHECK_EQ(MFXVideoDECODE_Query(_session, &_param, &_param), MFX_ERR_NONE);
+		auto initStatus = MFXVideoDECODE_Init(_session, &_param);
+		if (initStatus == MFX_WRN_PARTIAL_ACCELERATION)
+		{
+			;
+		}
+		else
+		{
+			L_CHECK_EQ(initStatus, MFX_ERR_NONE);
+		}
+		
 		mfxFrameSurface1 buffer;
 		memset(&buffer, 0, sizeof(mfxFrameSurface1));
 
@@ -113,6 +143,7 @@ namespace Base
 		
 		mfxFrameSurface1* output;
 		L_CHECK_EQ(MFXVideoDECODE_DecodeFrameAsync(_session, &bitstream, &buffer, &output, &_syncPoint), MFX_ERR_NONE);
+		L_CHECK(_syncPoint);
 		L_CHECK_EQ(output, &buffer);
 	}
 
@@ -133,7 +164,11 @@ namespace Base
 
 	void IntelGraphicsJpegDecoder::decode(void* buffer)
 	{
-		L_CHECK_EQ(MFXVideoCORE_SyncOperation(_session, _syncPoint, INFINITE), MFX_ERR_NONE);
+		auto status = MFXVideoCORE_SyncOperation(_session, _syncPoint, 6000);
+		if (status != MFX_ERR_NONE)
+		{
+			std::cout << status << std::endl;
+		}
 		L_CHECK_EQ(MFXVideoDECODE_Close(_session), MFX_ERR_NONE);
 
 		AVPixelFormat sourceFormat;
--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
 
+#include <base/logging/interface.h>
+#include <base/logging/sinks/stdout.h>
+
 int main(int argc, char* argv[]) {
+	Base::Logging::addSink(new Base::Logging::StdOutSink());
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
